014a9e2bb56875b2f20162a784dd700fdb2e8005
Yash Sehgal <yashsehgal.work@gmail.com>
2022-09-16 12:30:24 +0000

    chore: battery terminal shape added (wip)

--- src/component/IOSBatteryPercentage.jsx
+++ src/component/IOSBatteryPercentage.jsx
@@ -15,13 +15,13 @@ export default function IOSBatteryPercentage() {
   }, [batteryPercentage]);
   return (
     <div className="ios-battery-percentage-wrapper flex flex-col items-center justify-start gap-3">
-      <div className="battery-wrapper flex flex-row items-center justify-center">
-        <div className="battery-fill-container w-40 h-16 border-4 border-white rounded-lg p-1 rounded-xl flex flex-row items-center justify-center">
+      <div className="battery-wrapper flex flex-row items-center justify-center relative">
+        <div className="battery-fill-container w-40 h-16 border-4 border-white rounded-lg p-1 bg-zinc-900 rounded-xl flex flex-row items-center justify-center">
           <div className={`battery-fill w-full rounded-md h-full flex flex-row items-center justify-center ${batteryFillColor}`}>
             <span className={`battery-percentage font-bold text-2xl ${batteryFillColor === "bg-white" ? "text-zinc-700" : "text-white" }`}>{batteryPercentage}</span>
           </div>
         </div>
-        {/* <div className="battery-point-container w-4 h-4 rounded-full bg-white"></div> */}
+        <div className="battery-point-container w-4 h-4 rounded-full bg-white -ml-2 absolute -right-2"></div>
       </div>
       <input type="range" defaultValue={50} className="mt-4" onChange={(batterySlideEvent) => setBatteryPercentage(batterySlideEvent.target.value)} />
     </div>
